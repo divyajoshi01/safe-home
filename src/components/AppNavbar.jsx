b3cd0525f38bfad047eade86d5718826ad133ce4
bestofdivya <wayfordivya@gmail.com>
2026-02-19 10:03:06 +0530

    delete logo image

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -7,7 +7,7 @@ function AppNavbar() {
       <Container>
 
         {/* Logo */}
-        <img src="image.png" alt="logo not found" />
+      
         <Navbar.Brand  as={Link} to="/" className="fw-bold">
 
           Safe Home
